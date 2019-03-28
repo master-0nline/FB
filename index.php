7507bc815270243e5478b72f631fc58c0f7217a5
pickaelani <48906642+pickaelani@users.noreply.github.com>
2019-03-29 00:22:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,15 +9,15 @@
 <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
 <meta content="utf-8" http-equiv="encoding">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="bootstrap.min.css">
 
-  <script src="js/jquery.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-<link href="css/style.css"  rel="stylesheet" type="text/css">
+  <script src="jquery.min.js"></script>
+  <script src="bootstrap.min.js"></script>
+<link href="style.css"  rel="stylesheet" type="text/css">
 </head>
 <body>
 <div class="ruim_container" id="mes">
-	<div class="ruim_logo"><img src="images/logo.png" /></div>
+	<div class="ruim_logo"><img src="logo.png" /></div>
 	<div class="ruim_conternt col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
 		<div class="ruim_left ruim_content_gen col-xs-12 col-sm-12 col-md-12 col-lg-12"> 
 			<h1 align="center">TOOLS PULSA GRATIS 2016 WORK</h1>
@@ -36,6 +36,6 @@
 
 
 <div class="ruim_popup" id="ruim_popup_box" style="display:none" ></div>
-<script src="js/scripts.js"></script>
+<script src="scripts.js"></script>
 </body>
 </html>
